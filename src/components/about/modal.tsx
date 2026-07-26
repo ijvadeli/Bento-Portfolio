272d6ab13e45ec0abf387be45109c5bbe9631a73
ijvadeli <idhvandelindt@icloud.com>
2026-07-26 13:24:09 +0200

    Tighter letter spacing

--- src/components/about/modal.tsx
+++ src/components/about/modal.tsx
@@ -15,22 +15,38 @@ export default function AboutModal() {
           }
         }}
       >
-        Read more<ChevronRight size={16}/>
+        Read more
+        <ChevronRight size={16} />
       </button>
       <dialog id="about_modal" className="modal">
         <div className="modal-box">
           <form method="dialog">
             {/* if there is a button in form, it will close the modal */}
             <button className="btn btn-sm btn-circle absolute right-2 top-2 shadow-lg">
-              <X size={16}/>
+              <X size={16} />
             </button>
           </form>
           <h3 className="text-xl md:text-3xl">About me</h3>
-          <p className="py-2">I'm IJsbrand van de Lindt. I live in the Netherlands and my hobbies are programming and gaming. I started with learning HTML and CSS in 2023, but when I was younger I was already messing around in Scratch. I later also build games on the platform Roblox. My main motivation for building products is seeing people enjoy using them.</p>
+          <p className="py-2">
+            I'm IJsbrand van de Lindt. I live in the Netherlands and my hobbies
+            are programming and gaming. I started with learning HTML and CSS in
+            2023, but when I was younger I was already messing around in
+            Scratch. I later also build games on the platform Roblox. My main
+            motivation for building products is seeing people enjoy using them.
+          </p>
 
-          <h4 className="text-lg md:text-2xl">Music</h4>
-          <p className="py-2">I also listen to a wide variety of music, think of bands like Pink Floyd, Metallica, Queen, Radiohead, Guns N' Roses, King Crimson, Weather Report, ELO and more. One of my favorite songs is November Rain by Guns N' Roses.</p>
-          <iframe allow="autoplay *; encrypted-media *;" height="150" className="w-full;overflow:hidden;background:transparent;" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/album/november-rain-2022-version/1644730026?i=1644730834"></iframe>
+          <h4 className="text-lg md:text-2xl pt-4">Hobbies</h4>
+          <p className="py-2">
+            Aside from programming I really enjoy gaming in my free time. Some
+            of my favorite games are Cyberpunk 2077 and Elden Ring. The part I
+            love most about these games is their story, exploration and
+            adventure. For Cyberpunk more the story and for Elden Ring the crazy
+            world building.
+          </p>
+          <p>
+            I also like watching anime and reading manga. My favorite series are
+            One Piece (My favorite), JoJo's Bizarre Adventure and Dragon Ball.
+          </p>
         </div>
       </dialog>
     </>
--- src/components/local-time/time.tsx
+++ src/components/local-time/time.tsx
@@ -3,7 +3,7 @@ import LocalTime from "./script/local-time";
 
 export default function Time() {
   return (
-    <div className="h-full flex flex-col items-center justify-center">
+    <div className="h-full flex flex-col items-center justify-center py-4">
       <div className="inline-flex items-center gap-1.5">
         <div className="inline-grid *:[grid-area:1/1]">
           <div className="status status-info animate-ping"></div>
